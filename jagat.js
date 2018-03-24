4c0808c1288abb88ea0cfe4a1e70add42ee888c3
Jagatveer Singh <jagatveer.singh@sourcefuse.com>
2018-03-24 16:15:05 +0530

    making the same code as a feature toggle

--- jagat.js
+++ jagat.js
@@ -1,11 +1,23 @@
+var config = require('./config.json');
+const LaunchDarkly = require('ldclient-node');
+ldclient = LaunchDarkly.init(config.development.ldc_key)
 var jagat = "ahem";
-
 var status = "married";
 
-if(status == "single")
-{
-	jagat = 'I am the king';
-}
-
-
-console.log(jagat);
+ldclient.once('ready', function() {
+  ldclient.variation('temp', {key: 'jagatveer.singh@sourcefuse.com'}, false, function(err, showFeature) {
+    if (showFeature) {
+      if(status == "single")
+      {
+        jagat = 'I am the king';
+      }
+      console.log(jagat);
+    } else {
+      if(status == "married")
+      {
+        jagat = 'I could be a king';
+      }
+      console.log(jagat);
+    }
+  });
+});
